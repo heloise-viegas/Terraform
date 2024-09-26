27b000bd642159239875ce0ac60b915a78b5c46e
heloise <37453877+heloise-viegas@users.noreply.github.com>
2024-09-26 18:04:09 +0530

    Update cdn.tf

--- aws_s3_cdn/cdn.tf
+++ aws_s3_cdn/cdn.tf
@@ -1,27 +1,33 @@
 locals {
-  s3_origin_id = "${var.s3_name}-origin"
-  s3_domain_name = "${var.s3_name}-s3-website-${var.region}.amazonaws.com"
+  s3_origin_id   = "${var.s3_name}-origin"
+  s3_domain_name = aws_s3_bucket.s3.bucket_regional_domain_name
+  #"${var.s3_name}-s3-website-${var.region}.amazonaws.com"
 }
+
+resource "aws_cloudfront_origin_access_identity" "s3_distribution" {}
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name              = local.s3_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
     origin_id                = local.s3_origin_id
+
+     s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.s3_distribution.cloudfront_access_identity_path
+    }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "Some comment"
+  #comment             = "Some comment"
   default_root_object = "index.html"
 
-  logging_config {
-    include_cookies = false
-    bucket          = "mylogs.s3.amazonaws.com"
-    prefix          = "myprefix"
-  }
+  # logging_config {
+  #   include_cookies = false
+  #   bucket          = "mylogs.s3.amazonaws.com"
+  #   prefix          = "myprefix"
+  # }
 
-  aliases = ["mysite.example.com", "yoursite.example.com"]
+  # aliases = ["mysite.example.com", "yoursite.example.com"]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -59,4 +65,18 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+
+custom_error_response {
+    error_code            = 403
+    error_caching_min_ttl = 30
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+  custom_error_response {
+    error_code            = 400
+    error_caching_min_ttl = 30
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
 }
