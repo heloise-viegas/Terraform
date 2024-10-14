cb4ea33de1ce1275fdfee1d4e9038ffd5c2f3f11
heloise <37453877+heloise-viegas@users.noreply.github.com>
2024-10-14 21:07:17 +0530

    ci: updated bucket policy

--- aws_resources/S3/main.tf
+++ aws_resources/S3/main.tf
@@ -12,23 +12,47 @@ resource "aws_s3_bucket_acl" "s3_acl" {
     acl ="private"
 }
 
-resource "aws_s3_bucket" "s3_log" {
-  bucket = var.s3_log_name
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.s3.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
 
-  tags = {
-    Name        = var.s3_tag
-    Environment = var.s3_env
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.s3.arn}/*",
+    ]
   }
 }
 
-resource "aws_s3_bucket_acl" "s3_log_acl" {
-    bucket = aws_s3_bucket.s3_log.id
-    acl ="log-delivery-write"
-}
+# resource "aws_s3_bucket" "s3_log" {
+#   bucket = var.s3_log_name
 
-resource "aws_s3_bucket_logging" "logs" {
-  bucket = aws_s3_bucket.s3.id
+#   tags = {
+#     Name        = var.s3_tag
+#     Environment = var.s3_env
+#   }
+# }
 
-  target_bucket = aws_s3_bucket.s3_log.id
-  target_prefix = "log/"
-}
+# resource "aws_s3_bucket_acl" "s3_log_acl" {
+#     bucket = aws_s3_bucket.s3_log.id
+#     acl ="log-delivery-write"
+# }
+
+# resource "aws_s3_bucket_logging" "logs" {
+#   bucket = aws_s3_bucket.s3.id
+
+#   target_bucket = aws_s3_bucket.s3_log.id
+#   target_prefix = "log/"
+# }
